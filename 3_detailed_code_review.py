f7f6c06ab2d85aa843292b776b4c9f533fdafa60
Vinay Arjun Hajare <106072427+VinayHajare@users.noreply.github.com>
2024-07-17 11:44:28 +0530

    Update 3_detailed_code_review.py

--- 3_detailed_code_review.py
+++ 3_detailed_code_review.py
@@ -6,6 +6,9 @@ def display_chat(messages):
         st.chat_message(msg["role"], avatar="🥷" if msg["role"] == "assistant" else "👨🏻‍💻").markdown(msg["content"])
 
 
+def review():
+    yield st.session_state.detailed_review
+
 def detailed_code_review_tab():
     st.logo(image="img.png", icon_image="img.png")
     st.title("Detailed Code Review :memo:")
@@ -21,8 +24,8 @@ def detailed_code_review_tab():
     else:
         if st.button("Review :mag_right:"):
             with st.chat_message("assistant", avatar="🥷"):
-                st.write_stream(stream_text_word_by_word(st.session_state.detailed_review))
-                
+                #st.write_stream(stream_text_word_by_word(st.session_state.detailed_review))
+                st.write_stream(review())
                 st.session_state.messages.append({"role": "assistant", "content": st.session_state.detailed_review, "type": "review"})
 
             st.success("Code reviewed successfully.")
