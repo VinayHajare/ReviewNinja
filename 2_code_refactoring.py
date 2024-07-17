d4bb6af01817998d4dc7da44c19207e2bee2882a
Vinay Arjun Hajare <106072427+VinayHajare@users.noreply.github.com>
2024-07-17 11:43:05 +0530

    Update 2_code_refactoring.py

--- 2_code_refactoring.py
+++ 2_code_refactoring.py
@@ -6,6 +6,10 @@ def display_chat(messages):
         st.chat_message(msg["role"], avatar="🥷" if msg["role"] == "assistant" else "👨🏻‍💻").markdown(msg["content"])
 
 
+def refactor():
+    yield = st.session_state.refactored_code
+
+
 def code_refactoring_tab():
     st.logo(image="img.png", icon_image="img.png")
     st.title("Code Refactoring :hammer_and_wrench:")
@@ -21,7 +25,8 @@ def code_refactoring_tab():
     else:
         if st.button("Refactor :repeat:"):
             with st.chat_message("assistant", avatar="🥷"):
-                st.write_stream(stream_text_word_by_word(st.session_state.refactored_code))
+                #st.write_stream(stream_text_word_by_word(st.session_state.refactored_code))
+                st.write_stream(refactor())
                 st.session_state.messages.append({"role": "assistant", "content": st.session_state.refactored_code, "type": "refactored"})
 
             st.success("Code refactored successfully.")
