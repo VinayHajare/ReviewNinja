e22ec2860239f34af59fbc7cd7c1e490111a1128
Vinay Arjun Hajare <106072427+VinayHajare@users.noreply.github.com>
2025-11-02 10:10:24 +0530

    Implement model selection dropdown in sidebar
    Added model selection functionality to the sidebar.

--- review_ninja.py
+++ review_ninja.py
@@ -11,13 +11,32 @@ if "refactored_code" not in st.session_state:
     st.session_state.refactored_code = ""
 if "detailed_review" not in st.session_state:
     st.session_state.detailed_review = ""
+if "selected_model" not in st.session_state:
+    st.session_state.selected_model = "Meta-Llama-3-8B-Instruct-v2.Q6_K.gguf"  # Default model
+
+# Model selection dropdown
+available_models = [
+   "Meta-Llama-3-8B-Instruct-v2.Q6_K.gguf",
+   "codeqwen-1_5-7b-chat-q6_k.gguf",
+   "Codestral-22B-v0.1-Q6_K.gguf",
+   "AutoCoder-Q6_K.gguf"
+]
 
 st.logo(image="img.png", icon_image="img.png")
 st.set_page_config(page_title="ReviewNinja", page_icon=":ninja:", layout="wide")
 with st.sidebar:
     st.title("ReviewNinja 🥷🏻")
     st.caption("Streamlining Your Code Review Process")
+    selected_model = st.selectbox(
+        "Select Model:",
+        options=available_models,
+        index=available_models.index(
+            st.session_state.selected_model) if st.session_state.selected_model in available_models else 0,
+        key="model_selector"
+    )
 
+    # Update session state when model selection changes
+    st.session_state.selected_model = selected_model
 
 def main():
     home_page = st.Page("0_home.py", title="Home", icon="🏠")
@@ -32,4 +51,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
